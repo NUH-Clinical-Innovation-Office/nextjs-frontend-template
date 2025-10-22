3584d87463234fdd3afef5fd9d26f1276bb220a7
Yong Cheng Low <lowyongcheng@hotmail.com>
2025-10-22 23:18:55 +0800

    refactor: adjust component folder to use atomic design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
-import Link from 'next/link';
-import { ModeToggle } from '@/components/mode-toggle';
+import { ExternalLink } from '@/components/atoms/external-link';
+import { ModeToggle } from '@/components/molecules/mode-toggle';
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
@@ -41,11 +41,7 @@ export default function Home() {
 
         <div className="flex gap-4 items-center flex-col sm:flex-row w-full">
           <Button asChild size="lg" className="w-full sm:w-auto">
-            <Link
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <ExternalLink href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app">
               <Image
                 className="dark:invert"
                 src="/vercel.svg"
@@ -54,49 +50,33 @@ export default function Home() {
                 height={20}
               />
               Deploy now
-            </Link>
+            </ExternalLink>
           </Button>
           <Button asChild variant="outline" size="lg" className="w-full sm:w-auto">
-            <Link
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <ExternalLink href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app">
               Read our docs
-            </Link>
+            </ExternalLink>
           </Button>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <Button asChild variant="link" size="sm">
-          <Link
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <ExternalLink href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app">
             <Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
             Learn
-          </Link>
+          </ExternalLink>
         </Button>
         <Button asChild variant="link" size="sm">
-          <Link
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <ExternalLink href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app">
             <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
             Examples
-          </Link>
+          </ExternalLink>
         </Button>
         <Button asChild variant="link" size="sm">
-          <Link
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <ExternalLink href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app">
             <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
             Go to nextjs.org →
-          </Link>
+          </ExternalLink>
         </Button>
       </footer>
     </div>
